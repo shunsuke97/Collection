c66aa23edae0912e07128ee07e0f97d0ea1a470e
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-05-20 16:51:30 +0900

    update_createposts_migration

--- db/migrate/20200520071745_create_posts.rb
+++ db/migrate/20200520071745_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string  :text,              null: false
+      t.string  :title,             null: false
+      t.string  :image,             null: false
+      t.string  :author,            null: false
+      t.text    :introduction,      null: false
+      t.integer :user_id,           null: false
       t.timestamps
     end
   end
