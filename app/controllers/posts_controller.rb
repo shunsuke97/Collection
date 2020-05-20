c22160485a2c2e5870eca4202064717d4802505d
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-05-20 21:03:55 +0900

    update_strong_parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,11 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text, :title, :image, :author, :introduction, user_id: current_user.id)
   end
 end
