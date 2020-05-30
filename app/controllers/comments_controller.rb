5538aacc159b51814804461aba5a0ffdcb373760
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-05-30 21:36:04 +0900

    fixed_create_action_to_comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(text: comment_params[:text], post_id: post_params[:post_id], user_id: current_user.id)
+    comment = Comment.create(text: comment_params[:text], post_id: comment_params[:post_id], user_id: current_user.id)
     redirect_to "/posts/#{comment.post.id}"
   end
 
