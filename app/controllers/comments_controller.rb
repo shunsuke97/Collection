4fd305ca801fc31973bbcb5fc4db1c0bac5c5a4f
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-05-30 17:52:09 +0900

    add_strong_parameters_to_comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,9 @@ class CommentsController < ApplicationController
   def create
     Comment.create(text: params[:text], post_id: params[:post_id], user_id: current_user.id)
   end
+
+  private
+  def comment_params
+    params.permit(:text, :post_id)
+  end
 end
