fd0c969b0788665c322637bd9bb18316ac47e875
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-02 00:18:42 +0900

    update_routes.rb
    一見何を指すのか分かりにくいルーティングに、コメントアウトを付与した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    # deviseのregistrations_controllerにusers/registrations_controllerを参照させる
     registrations: 'users/registrations'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  
+
   root "top#index"
   resources :posts do
     resources :comments, only: [:create]
