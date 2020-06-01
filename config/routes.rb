4d849068ab2bd0567b8cb15941b2974575c05764
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-02 00:14:33 +0900

    update_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  
   root "top#index"
   resources :posts do
     resources :comments, only: [:create]
