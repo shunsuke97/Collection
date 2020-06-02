db1895d13c6160a8efb15a3463fc47a4e7c5466e
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-02 16:27:16 +0900

    delete_validation_to_post.rb
    画像に対して、文字数を指定する余計なバリデーションがかかっていたため削除。
    createアクションを動かしたときに、画像を含む内容がDBに保存されなくなってしまっていた。

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
 
   validates :text,            length: { maximum: 50 } ,  presence: true
   validates :title,           length: { maximum: 20 } ,  presence: true
-  validates :image,           length: { maximum: 30 } ,  presence: true
+  validates :image,                                      presence: true
   validates :author,          length: { maximum: 20 } ,  presence: true
   validates :introduction,    length: { maximum: 120 } , presence: true
 
