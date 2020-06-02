025872f5e5ff61db439fe6882345027fc53c108a
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-02 14:23:58 +0900

    update_image_uploader.rb
    画像のリサイズを設定。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  # 画像の縦横比を維持したまま、widthを最大200px、heightを最大324pxとする
+  process resize_to_fit: [200, 324]
+
+  # 投稿された画像はuploadの中に保存される
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
