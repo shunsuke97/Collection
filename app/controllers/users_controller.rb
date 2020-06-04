d5615d6f49af2b896eaeb82438f3ee924a3b1042
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-05 04:02:24 +0900

    add_link_and_users/show
    current_userにすると、誰の名前を押しても自分のマイページに飛んでしまう状態だったので、それを修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @name = current_user.name
-    @posts = Post.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC") 
+    user = User.find(params[:id])
+    @name = user.name
+    @posts = user.posts.page(params[:page]).per(5).order("created_at DESC")
   end
 end
