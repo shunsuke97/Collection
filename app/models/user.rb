9a3fbd72dd9cd9de895aa026a5cf245725955605
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-08 23:58:17 +0900

    add_method_update_resource
    現状では編集画面にて「パスワードは変更したくないのに、パスワードも入力しないと他が変更できない」という状態になってしまっていたため、「パスワードは変更したい場合のみ入力すれば良い」という状態に変更した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true, uniqueness: true
+
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+    # "パスワード変更のためのパスワード入力フィールド"と"その確認フィールド"の両方とも空の場合のみ、パスワードなしで更新できるようにする
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
