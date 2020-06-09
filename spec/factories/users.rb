4182250f869e2c5448f62dfa9163b7d4f2dfbcc8
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-10 03:36:26 +0900

    create_factories/users.rb
    spec/factories/users.rbにダミーのuser情報を追加。

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    name                  {"ネコちゃん"}
+    email                 {"nekochan@gmail.com"}
+    password              {"aaaaaa"}
+    password_confirmation {"aaaaaa"}
+  end
+
+end
