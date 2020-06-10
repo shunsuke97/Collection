cdb64fddc2bedae2328fcb6df7da5c293f9b30e7
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-11 01:14:27 +0900

    update_users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
-
   factory :user do
     name                  {"ネコちゃん"}
     email                 {"nekochan@gmail.com"}
     password              {"123456"}
     password_confirmation {"123456"}
   end
-
 end
