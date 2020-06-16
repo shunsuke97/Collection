cafd365845c4b3d1fd8a2d95e5fd4ca073a8a230
shunsuke97 <53967112+shunsuke97@users.noreply.github.com>
2020-06-16 19:03:59 +0900

    change_post_validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many   :comments
 
-  validates :text,            length: { maximum: 50 } ,  presence: true
+  validates :text,            length: { maximum: 60 } ,  presence: true
   validates :title,           length: { maximum: 20 } ,  presence: true
-  validates :author,          length: { maximum: 20 } ,  presence: true
-  validates :introduction,    length: { maximum: 120 } , presence: true
+  validates :author,          length: { maximum: 12 } ,  presence: true
+  validates :introduction,    length: { maximum: 130 } , presence: true
   validates :image,                                      presence: true
 
   mount_uploader :image, ImageUploader
